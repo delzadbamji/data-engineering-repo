94278a57bd9b7e63b81c12ec321afa223d53f300
Delzad Bamji <delzadbamji@gmail.com>
2022-03-15 21:14:33 -0400

    Create app.py

---
+++ merge-and-reconstruct/app.py
@@ -0,0 +1,44 @@
+import os
+import glob
+import pandas as pd
+# os.chdir(".")
+#
+extension = 'csv'
+from tqdm import tqdm
+
+#####################################
+''' --------------------------------- Monthly ---------------------------------------'''
+#####################################
+
+def get_monthly(file_directory,year,output_loc):
+  
+ '''
+  Data from the csv is structured and reconstructed in a monthly fashion.
+ '''
+
+#####################################
+''' --------------------------------- Weekly ---------------------------------------'''
+#####################################
+
+def get_weekly(file_directory,year,output_loc):
+  
+ '''
+  Data from the csv is structured and reconstructed in a weekly fashion.
+ '''
+
+
+#####################################
+''' -----------------------------merge year_1 vs year_2 -------------------------------------------'''
+#####################################
+def merge_years(year1_df,year2_df):
+
+
+#####################################
+''' -----------------------------count passengers -------------------------------------------'''
+#####################################
+
+def count_passengers(year_df):
+  '''
+  count the passengers for each year
+  '''
+
