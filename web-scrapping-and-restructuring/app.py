7706d79a8f28fa3f3f322812d1b181c5c7fab02a
Delzad Bamji <delzadbamji@gmail.com>
2022-03-15 23:13:38 -0400

    Create app.py

---
+++ web-scrapping-and-restructuring/app.py
@@ -0,0 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+result = requests.get("website string")
+
+# print(result.status_code)
+def consolidate_data(content):
+  '''
+  Web scrap, clean, consolidate, and structure in a tabular csv format 
+  '''
+  
+def reconstruct_and_create(file,year,output_file):
+  '''
+  reformat, clean, consolidate, and structure in a tabular csv format  
+  '''
