c3c3c474cdb7ee458228f01626f9f070c136e948
Delzad Bamji <delzadbamji@gmail.com>
2022-03-15 23:17:29 -0400

    Update app.py

--- web-scrapping-and-restructuring/app.py
+++ web-scrapping-and-restructuring/app.py
@@ -6,11 +6,13 @@ result = requests.get("website string")
 # print(result.status_code)
 def consolidate_data(content):
   '''
+  Project 1
   Web scrap, clean, consolidate, and structure in a tabular csv format 
   '''
   
 def reconstruct_and_create(file,year,output_file):
   '''
+  Project 1
   reformat, clean, consolidate, and structure in a tabular csv format  
   '''
 
